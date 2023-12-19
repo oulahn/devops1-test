7f61761da7618d29a5212988e02d9bb6cab30ddb
oulahn <oula.hnaino@gmail.com>
2023-12-19 22:52:00 +0000

    run test and report

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-
-let server = "http://webapp";
+let should = chai.should(); 
+let server = "http://18.181.184.53/getmessage.php"; // "http://webapp";
 chai.use(chaiHttp);
 
 describe('/getmessage.php', () => {
     it('it should GET the hello message', (done) => {
         chai.request(server)
             .get('/getmessage.php')
-            .send(book)
+//            .send(book)
             .end((err, res) => {
+
+                if (err) {
+                    console.log('Response status:', err.status);
+                    console.log('Response body:', err.body);
+                }
                 res.should.have.status(200);
                 res.body.should.be.a('object');
                 res.body.should.have.property('response');
-                res.body.should.have.property('response').eql('hello');
+                res.body.response.should.eql('hello');
+//                res.body.should.have.property.eql('hello');
                 done();
-            });
+           });
     });
 });
